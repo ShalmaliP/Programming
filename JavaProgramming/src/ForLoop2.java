ca9ce694a5e1cf6a57c04e3e4235f22a1002bf2c
Shalmali Pradhan <pradhan.shalmali@gmail.com>
2017-07-24 14:52:44 -0700

    For Loop Example 2

---
+++ JavaProgramming/src/ForLoop2.java
@@ -0,0 +1,13 @@
+/*
+ * 0 1 2 3 4 5 6 7 8 9 10
+ */
+public class ForLoop2 
+{
+	public static void main(String[] args)
+	{
+		for(int i = 0; i <= 10; i++)
+		{
+			System.out.print(i + " ");
+		}
+	}
+}
